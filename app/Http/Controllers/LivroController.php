1783c05307cbef24f7e9bd5aef6818c76b906f32
Carlos W. Gama <carloswgama@gmail.com>
2018-04-13 00:12:41 -0300

    Já baixar com o Laravel

---
+++ app/Http/Controllers/LivroController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LivroController extends Controller
+{
+
+    public function listar() { }
+
+    public function cadastrar() {  }
+
+    public function visualizar($id) {   }
+
+    public function editar($id) {    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'livros'], function() {
+    Route::get('/', 'LivroController@listar')->name('livros.listar');
+    Route::get('/novo', 'LivroController@cadastrar')->name('livros.cadastrar');
+    Route::get('/{id}', 'LivroController@visualizar')->name('livros.visualizar');
+    Route::get('/editar/{id}', 'LivroController@editar')->name('livros.editar');
+});
+
+
